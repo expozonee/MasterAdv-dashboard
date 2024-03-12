cc0de2a1cf62b15c927e5769a00f25cc4c2e907e
Ibrahem <boss.abrahem@hotmail.com>
2024-03-12 16:38:00 +0200

    Updated the middleware file

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,32 @@
 import { NextResponse, NextRequest } from "next/server";
-import PageData from "@/app/dashboard/PageData";
+import { getTitles } from "@/app/api/route";
+
+interface Data {
+  names: [{ name: string }];
+  urls: string[];
+}
 
 export default async function middleware(request: NextRequest) {
-  console.log();
-  // const checkData = PageData();
   const req = request;
-  // console.log(req.url.);
-  // console.log("hello this is middleware!");
+  const path = req.url.split("http://localhost:3000/")[1];
+  const pathArray = path.split("/");
+  const slugs = pathArray.slice(1);
+  // console.log(slugs);
 
-  return NextResponse.next(); // Add this line
+  async function getTitle() {
+    const data: Data = await getTitles(slugs);
+    return data;
+  }
+  const { names, urls } = await getTitle();
+  const isInvalidUrl = names.some((name) => name === null);
+
+  if (isInvalidUrl) {
+    const url = req.nextUrl.clone();
+    url.pathname = "/dashboard";
+    return NextResponse.redirect(url);
+  } else {
+    return NextResponse.next();
+  }
 }
 
 export const config = {
