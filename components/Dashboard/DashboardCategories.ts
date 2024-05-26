12aecaebd6741a4a3e02432e0d42c158a8999fc4
Ibrahem <boss.abrahem@hotmail.com>
2024-05-26 22:05:10 +0300

    Updated the headers in next config file

--- components/Dashboard/DashboardCategories.ts
+++ components/Dashboard/DashboardCategories.ts
@@ -1,11 +1,15 @@
 export default async function fetchCategories() {
-  const categoriesData = await fetch("https://masteradv.vip/api/categories", {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-cache",
-    },
-  });
-  const { data: categories } = await categoriesData.json();
-  return categories;
+  try {
+    const categoriesData = await fetch("https://masteradv.vip/api/categories", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache",
+      },
+    });
+    const { data: categories } = await categoriesData.json();
+    return categories;
+  } catch (error) {
+    console.error(error);
+  }
 }
