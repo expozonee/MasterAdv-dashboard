ca9cf9969d1db4d5c75202daef5c196c053e6e1d
Ibrahem <boss.abrahem@hotmail.com>
2024-05-28 08:57:15 +0300

    handled the method type 'options' in the api request

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,4 +1,16 @@
 export async function GET(request: Request) {
+  const { method } = request;
+
+  if (method === "OPTIONS") {
+    return new Response(null, {
+      headers: {
+        "Access-Control-Allow-Origin": "https://masteradv.vip/",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
+      },
+    });
+  }
+
   const response = await fetch(
     "https://masteradv-backend.vercel.app/categories",
     {
