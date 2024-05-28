188dca17da861448c6dbf84ef1d608277e048e06
Ibrahem <boss.abrahem@hotmail.com>
2024-05-28 09:02:29 +0300

    Updated the request headers in api route handler for categories

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,16 +1,4 @@
 export async function GET(request: Request) {
-  const { method } = request;
-
-  if (method === "OPTIONS") {
-    return new Response(null, {
-      headers: {
-        "Access-Control-Allow-Origin": "https://masteradv.vip/",
-        "Access-Control-Allow-Methods": "GET",
-        "Access-Control-Allow-Headers": "Content-Type",
-      },
-    });
-  }
-
   const response = await fetch(
     "https://masteradv-backend.vercel.app/categories",
     {
@@ -18,6 +6,9 @@ export async function GET(request: Request) {
       headers: {
         "Content-Type": "application/json",
         "Cache-Control": "no-cache",
+        "Access-Control-Allow-Origin": "https://masteradv.vip/",
+        "Access-Control-Allow-Methods": "GET",
+        "Access-Control-Allow-Headers": "Content-Type",
       },
     }
   );
