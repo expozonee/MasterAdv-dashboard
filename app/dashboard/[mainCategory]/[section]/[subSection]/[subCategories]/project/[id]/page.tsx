5fba3941d5cf31763268578912f07d3dfd7c0a8a
Ibrahem <boss.abrahem@hotmail.com>
2024-06-13 19:30:29 +0300

    added the logic for fetching the required projects for each page

--- app/dashboard/[mainCategory]/[section]/[subSection]/[subCategories]/project/[id]/page.tsx
+++ app/dashboard/[mainCategory]/[section]/[subSection]/[subCategories]/project/[id]/page.tsx
@@ -6,53 +6,72 @@ import { getPortfolioSections } from "@/utils/data";
 import { Divider } from "@mui/material";
 import { usePathname } from "next/navigation";
 import WhatsappShareButton from "@/components/shareButtons/WhatsappShareButton";
+import { useEffect, useState } from "react";
+import getProjectsDashboard from "@/utils/getProjectsDashboard";
+import type { Project } from "@/types/project/Project";
+import { useQuery } from "react-query";
+import ProjectSkeleton from "@/components/Skeletons/ProjectSkeleton";
+import DashboardProjectSkeleton from "@/components/Skeletons/DashboardProjectSkeleton";
 
 type ProjectPageProps = {
   params: {
+    id: string;
     mainCategory: string;
     section: string;
     subSection: string;
     subCategories: string;
-    id: number;
   };
 };
 
 const ArimoFontTitle = Arimo({ subsets: ["hebrew"], weight: ["700"] });
 const ArimoFontText = Arimo({ subsets: ["hebrew"], weight: ["400"] });
 
-export default function ProjectPage({
-  params: { mainCategory, section, subCategories, subSection, id },
-}: ProjectPageProps) {
-  console.log(mainCategory, section, subSection, subCategories);
-  console.log(id);
-  const pathname = usePathname();
+export default function ProjectPage({ params }: ProjectPageProps) {
+  const [project, setProject] = useState<Project | undefined>(undefined);
 
-  const imageData = getPortfolioSections().find((image) => image.id == id);
-  console.log(imageData);
+  const { isLoading, isError } = useQuery({
+    queryKey: ["projects", params],
+    queryFn: async () => {
+      return await getProjectsDashboard(params);
+    },
+    onSuccess: (data: Project[]) => {
+      setProject(data.find((project) => project.itemId == params.id));
+    },
+  });
 
-  return imageData ? (
-    <div>
-      <div className="grid gap-10 ms:grid-cols-1 xl:grid-cols-2">
-        <div className="w-full">
-          <Image
-            className="rounded-xl drop-shadow-lg"
-            src={imageData.imageUrl}
-            alt={imageData.title}
-            width={500}
-            height={500}
-            style={{ height: "100%", width: "100%" }}
-          />
-        </div>
+  return (
+    <>
+      {/* {isLoading && <p>Loading...</p>} */}
+      {isError && <p>Error</p>}
+      {isLoading ? (
+        <DashboardProjectSkeleton />
+      ) : (
         <div>
-          <h1 className={`text-6xl ${ArimoFontTitle.className}`}>
-            {imageData.title}
-          </h1>
-          <WhatsappShareButton pathname={pathname} />
+          <div className="grid gap-10 ms:grid-cols-1 xl:grid-cols-2">
+            <div className="w-full">
+              {project && (
+                <Image
+                  className="rounded-xl drop-shadow-lg"
+                  src={project.imageUrl}
+                  alt={project.imageUrl}
+                  width={500}
+                  height={500}
+                  style={{ height: "100%", width: "100%" }}
+                />
+              )}
+            </div>
+            <div>
+              {/* <h1 className={`text-6xl ${ArimoFontTitle.className}`}>
+            {project.title}
+          </h1> */}
+              <WhatsappShareButton categories={params} />
+            </div>
+          </div>
+          {/* <p className="flex justify-center items-center my-25">
+            Design is Fun
+          </p> */}
         </div>
-      </div>
-      <p className="flex justify-center items-center my-25">Design is Fun</p>
-    </div>
-  ) : (
-    <div>Not found</div>
+      )}
+    </>
   );
 }
