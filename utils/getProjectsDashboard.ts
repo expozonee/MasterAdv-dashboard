94d00cb2498b59eb0658b14e6459669676898c61
Ibrahem <boss.abrahem@hotmail.com>
2024-06-13 19:32:15 +0300

    created file for handling the logic for fetching the projects from the backend using the api in route handler

---
+++ utils/getProjectsDashboard.ts
@@ -0,0 +1,28 @@
+import axios from "axios";
+
+type getProjectsDashboardProps = {
+  mainCategory: string;
+  section: string;
+  subSection: string;
+  subCategories: string;
+};
+
+export default async function getProjectsDashboard({
+  mainCategory,
+  section,
+  subSection,
+  subCategories,
+}: getProjectsDashboardProps) {
+  const result = await axios.get(`http://localhost:3000/api/projects`, {
+    headers: {
+      "Cache-Control": "no-cache",
+    },
+    params: {
+      mainCategory: mainCategory,
+      section: section,
+      subSection: subSection,
+      subCategories: subCategories,
+    },
+  });
+  return result.data;
+}
