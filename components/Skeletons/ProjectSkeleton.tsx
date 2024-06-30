6f5916570c9cbb92c38a9e139d9d051d037de0c0
Ibrahem <boss.abrahem@hotmail.com>
2024-06-30 12:08:15 +0300

    fixed the ProjectSkeleton design on mobile

--- components/Skeletons/ProjectSkeleton.tsx
+++ components/Skeletons/ProjectSkeleton.tsx
@@ -2,7 +2,15 @@ import { Skeleton } from "@mui/material";
 
 export default function ProjectSkeleton() {
   return (
-    <div className="flex gap-3">
+    <div
+      style={{
+        display: "grid",
+        gridAutoFlow: "dense",
+        gap: "1rem",
+        gridTemplateColumns: "repeat(auto-fit, minmax(200px, 400px))",
+      }}
+      className="justify-items-center "
+    >
       <Skeleton
         height={400}
         animation="wave"
