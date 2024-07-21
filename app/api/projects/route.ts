16681b044749bf78b8d63a6a7dff04c04cf27ac0
Ibrahem <boss.abrahem@hotmail.com>
2024-07-21 17:10:13 +0300

    updated the url to the deployed backend

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -3,12 +3,9 @@ import axios from "axios";
 export async function GET(request: Request) {
   const businessType = new URL(request.url).searchParams.get("businessType");
   const projectType = new URL(request.url).searchParams.get("projectType");
-  console.log("the request is: ", request);
 
-  // if (!subCategory) {
-  // console.log("from get", mainCategory, section, subSection, subCategory);
   const response = await axios
-    .get("http://localhost:4000/getProjects", {
+    .get("https://masteradv-backend.vercel.app/getProjects", {
       headers: {
         "Cache-Control": "no-cache",
       },
@@ -19,19 +16,4 @@ export async function GET(request: Request) {
     })
     .then((res) => res.data);
   return new Response(JSON.stringify(response));
-  // }
-
-  // const response = await axios.get("http://localhost:4000/getProjects", {
-  //   headers: {
-  //     "Cache-Control": "no-cache",
-  //   },
-  //   params: {
-  //     mainCategory: mainCategory,
-  //     section: section,
-  //     subSection: subSection,
-  //     subCategory: subCategory,
-  //   },
-  // });
-  // console.log(response);
-  // return new Response(JSON.stringify(response.data));
 }
