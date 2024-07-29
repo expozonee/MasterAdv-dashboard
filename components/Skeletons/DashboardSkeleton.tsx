2d3ec271f3d0609ad1b81149686f7b2a784186cd
Ibrahem <boss.abrahem@hotmail.com>
2024-07-29 12:19:38 +0300

    fixed the loading skeleton in the dashbaord page

--- components/Skeletons/DashboardSkeleton.tsx
+++ components/Skeletons/DashboardSkeleton.tsx
@@ -3,23 +3,7 @@ import DashboardCard from "../Dashboard/DashboardCard";
 
 export default function DashboardSkeleton() {
   return (
-    <div className="grid gap-2">
-      <Skeleton
-        animation="wave"
-        variant="rectangular"
-        width={"100%"}
-        sx={{ backgroundColor: "gray", opacity: 0.2, borderRadius: 2 }}
-      >
-        <DashboardCard />
-      </Skeleton>
-      <Skeleton
-        animation="wave"
-        variant="rectangular"
-        width={"100%"}
-        sx={{ backgroundColor: "gray", opacity: 0.2, borderRadius: 2 }}
-      >
-        <DashboardCard />
-      </Skeleton>
+    <div>
       <Skeleton
         animation="wave"
         variant="rectangular"
