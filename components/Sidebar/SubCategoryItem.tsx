5c93ee68b29995d51d9a40b68583cde0118eed8e
Ibrahem <boss.abrahem@hotmail.com>
2024-08-04 17:11:55 +0300

    changed the background color on hover to gold and text to white

--- components/Sidebar/SubCategoryItem.tsx
+++ components/Sidebar/SubCategoryItem.tsx
@@ -22,7 +22,7 @@ const SubCategoryItem = ({
       <Link
         href={href}
         className={`group relative flex items-center gap-2.5 rounded-xl py-2 px-4 font-medium text-bodydark1 duration-300 ease-in-out hover:bg-graydark dark:hover:bg-meta-4 ${
-          isActive && "bg-bodydark2 text-boxdark"
+          isActive && "bg-gold text-white"
         }`}
         onClick={() => onClick(href)}
       >
