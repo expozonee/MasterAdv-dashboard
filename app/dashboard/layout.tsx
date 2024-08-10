c078b0e7b0dcf9be15d6d41b30237e4b4d686e2f
Ibrahem <boss.abrahem@hotmail.com>
2024-08-10 17:43:16 +0300

    commented the SideBarProvider and moved it to the root layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -11,42 +11,42 @@ export default function RootLayout({
 }) {
   return (
     <CategoriesProvider>
-      <SideBarProvider>
-        <div className="bg-main">
-          {/* {loading ? (
+      {/* <SideBarProvider> */}
+      <div className="bg-main">
+        {/* {loading ? (
           <Loader />
         ) : ( */}
-          <div className="flex height-dvh overflow-hidden">
-            {/* <!-- ===== Sidebar Start ===== --> */}
-            <Sidebar />
-            <SidebarBackdrop />
-            {/* <!-- ===== Sidebar End ===== --> */}
+        <div className="flex height-dvh overflow-hidden">
+          {/* <!-- ===== Sidebar Start ===== --> */}
+          <Sidebar />
+          <SidebarBackdrop />
+          {/* <!-- ===== Sidebar End ===== --> */}
 
-            {/* <!-- ===== Content Area Start ===== --> */}
-            <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-              {/* <!-- ===== Header Start ===== --> */}
-              <Header />
-              {/* <!-- ===== Header End ===== --> */}
+          {/* <!-- ===== Content Area Start ===== --> */}
+          <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
+            {/* <!-- ===== Header Start ===== --> */}
+            <Header />
+            {/* <!-- ===== Header End ===== --> */}
 
-              {/* <!-- ===== Main Content Start ===== --> */}
-              <main
-                className="relative"
-                //  ref={mainRef.current}
+            {/* <!-- ===== Main Content Start ===== --> */}
+            <main
+              className="relative"
+              //  ref={mainRef.current}
+            >
+              <div
+                // ref={containerRef}
+                className="mx-auto max-w-screen-3xl p-4 md:p-6 2xl:p-6"
               >
-                <div
-                  // ref={containerRef}
-                  className="mx-auto max-w-screen-3xl p-4 md:p-6 2xl:p-6"
-                >
-                  {children}
-                </div>
-              </main>
-              {/* <!-- ===== Main Content End ===== --> */}
-            </div>
-            {/* <!-- ===== Content Area End ===== --> */}
+                {children}
+              </div>
+            </main>
+            {/* <!-- ===== Main Content End ===== --> */}
           </div>
-          {/* // )} */}
+          {/* <!-- ===== Content Area End ===== --> */}
         </div>
-      </SideBarProvider>
+        {/* // )} */}
+      </div>
+      {/* </SideBarProvider> */}
     </CategoriesProvider>
   );
 }
