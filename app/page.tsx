799bd00a09d8ee7ce8d05ed4cc14e64de5bffb01
Ibrahem <boss.abrahem@hotmail.com>
2024-08-10 17:51:35 +0300

    removed the commented code

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,6 @@ import DesignerInfo from "@/components/HomePage/DesignerInfo";
 import ProjectCarousel from "@/components/HomePage/Carousel";
 
 export default function Home() {
-  // const [isMobile, setIsMobile] = useState(false);
-
-  // useEffect(() => {
-  //   const handleResize = () => {
-  //     const width = window.innerWidth;
-  //     if (width <= 460) {
-  //       setIsMobile(true);
-  //     } else {
-  //       setIsMobile(false);
-  //     }
-  //   };
-
-  //   handleResize();
-
-  //   window.addEventListener("resize", handleResize);
-
-  //   return () => {
-  //     window.removeEventListener("resize", handleResize);
-  //   };
-  // }, []);
-
   return (
     <main
       style={{
